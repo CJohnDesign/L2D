199a829e4f04e0eccfcd5f01d7e0ab8900e70364
Chris J <cjohndesign@gmail.com>
2015-09-20 20:40:25 -0400

    multi sections
    Mobile broken. Paddingless and marginless grid in effect

--- _/js/script.js
+++
@@ -1,5 +0,0 @@
-$('.hamburger').on('click', function() {
-  $(this).toggleClass('open');
-  var openclose = $(this).hasClass('open') ? 'Close' : 'Open';
-  $(this).attr("title", openclose + ' Menu');
-});
